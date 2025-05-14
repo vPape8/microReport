f44da8f9e9639af71c4729e70df876bdb7977f4f
Martin Coronel <coronelmartin@hotmail.cl>
2025-05-13 22:42:59 -0400

    reporte

---
+++ src/main/java/com/cordy/reporte/ReporteApplication.java
@@ -0,0 +1,13 @@
+package com.cordy.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReporteApplication.class, args);
+    }
+
+}
